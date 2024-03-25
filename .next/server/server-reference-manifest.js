6112d799d4e4171b98a60c0cfbb3603def8de862
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-03-25 16:17:56 +0200

    adding skeleton and css modules

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gLtNl+PYIWU9j4u/a+BeI6Kuig8HbcQzguZvTpF+nlc=\"\n}"
---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import styles from './navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1 className={styles.textCenter}>БФЖАРГОН<span>.com</span></h1>
+    </nav>
+  );
+}
